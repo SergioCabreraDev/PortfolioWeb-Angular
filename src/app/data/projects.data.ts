9b2d36f2850ac6b085fc3a8b9f4236b758b97cb8
Sergio Cabrera <scabrera.main@gmail.com>
2024-08-23 14:35:37 +0200

    add project

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -4,6 +4,32 @@ import { Projects } from "../models/projects";
 export const projects: Projects[] = [
     {
         id: 1,
+        tittle: 'Web Full Stack Restaurante',
+        category: 'Back End y Front End',
+        description: 'Desarrollo de una aplicación web desplegada en AWS para un restaurante de hamburguesas utilizando Angular y Spring Boot. La web incluye funciones importantes como pedidos a domicilio simulando un sistema de compra con carrito, gestión de usuarios con roles, sistema de reservas y seguimiento en tiempo real de los pedidos.',
+        imgPortada: "../../assets/img/564shots_so.png",
+        urlProyect: "https://github.com/SergioCabreraDev/Web-Sistema-de-Gestion-de-Restaurante-Full-Stack",
+        technologies: [
+            {
+            name: "Spring Boot",
+            urlImg: "../../assets/img/spring-boot-svgrepo-com.png"
+        },
+        {
+            name: "Angular",
+            urlImg: "../../assets/img/brand-angular.png"
+        },
+        {
+            name: "SQL",
+            urlImg: "../../assets/img/sql.png"
+        },
+        {
+            name: "AWS",
+            urlImg: "../../assets/img/sql.png"
+        }
+    ]
+    },
+    {
+        id: 2,
         tittle: 'API REST de Control de Clientes',
         category: 'Back End',
         description: 'API REST para el control de clientes, desarrollado utilizando Spring Boot en el backend y JavaScript para el frontend, la API expone endpoints RESTful que permiten realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) sobre los datos de clientes. Tambien implementa un sistema de autenticación basado en tokens JWT y la encriptación de las contraseñas mediante algoritmos de hash como Argon2.',
@@ -25,7 +51,7 @@ export const projects: Projects[] = [
     ]
     },
     {
-        id: 2,
+        id: 3,
         tittle: 'Block House Web Responsive',
         category: 'Back End y Front End',
         description: 'Web basada en las funciones y servicios proporcionados por blockhouse.es, pero con un diseño más moderno y actualizado. La Web tiene un diseño Responsive, sistema de compra con carrito, reservas, cuentas de usuario con sesiones y registros. ',
@@ -55,7 +81,7 @@ export const projects: Projects[] = [
     ]
     },
     {
-        id: 3,
+        id: 4,
         tittle: 'Predicción de ataque al corazón',
         category: 'BI',
         description: 'Notebook con un modelo de predicción, con una precisión del 88%.',
@@ -69,7 +95,7 @@ export const projects: Projects[] = [
     ]
     },
     {
-        id: 4,
+        id: 5,
         tittle: 'Análisis de sentimientos',
         category: 'BI',
         description: 'Haciendo web scraping en Amazon, he recopilado cientos de comentarios para realizar un análisis de sentimientos con NLTK.',
@@ -83,7 +109,7 @@ export const projects: Projects[] = [
     ]
     },
     {
-        id: 5,
+        id: 6,
         tittle: 'Web de Formación Interactiva Block House',
         category: 'Front End',
         description: 'Desarrollé una plataforma web responsive dirigida a camareros, cocineros y ayudantes de camarero. Mi objetivo era ofrecer una experiencia dinámica y eficiente para visualizar y practicar el manejo de los números de los productos del restaurante.',
